d9a37f57ff62a1caa60c21be0037cad06f91ca85
Hamza Saif <hamzasaifworking@gmail.com>
2026-09-17 12:41:56 +0100

    asset that im not sure is necesary

---
+++ assets/tiles/Bob_Character.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="Bob_Character" tilewidth="16" tileheight="16" tilecount="336" columns="24">
+ <image source="../images/Characters/Bob_16x16.png" width="384" height="224"/>
+</tileset>
